b4f4c3febff6764fa33f98c55969f3120f3f6cae
Jong Hwan Hong <davihong@usp.br>
2023-09-15 12:01:56 -0300

    unix

--- sketch_jul24c/sketch_jul24c.ino
+++ sketch_jul24c/sketch_jul24c.ino
@@ -1,6 +1,13 @@
 #include <WiFi.h>
 #include "time.h"
 #include <PubSubClient.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <LoRa.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <TimeLib.h>
 
 // Replace with your network credentials
 const char* ssid = "tcclucas";
@@ -13,6 +20,19 @@ const char* mqttPassword = "862b7c31-7caf-4420-8dfd-2cca33c6f587";
 
 // NTP server to request epoch time
 const char* ntpServer = "pool.ntp.org";
+
+// /* GPIO do módulo WiFi LoRa 32(V2) que o pino de comunicação do sensor está ligado. */
+// #define DHTPIN    13 /* (GPIO 13) */
+
+// /* Definicoes para comunicação com radio LoRa */
+// #define SCK_LORA           5
+// #define MISO_LORA          19
+// #define MOSI_LORA          27
+// #define RESET_PIN_LORA     14
+// #define SS_PIN_LORA        18
+
+// #define HIGH_GAIN_LORA     20  /* dBm */
+// #define BAND               915E6  /* 915MHz de frequencia */
 
 // Variable to save current epoch time
 unsigned long epochTime; 
@@ -30,6 +50,13 @@ unsigned long getTime() {
   }
   time(&now);
   return now;
+}
+
+// Setup unix
+void printTime(time_t timeInput, char* output) {
+  // Format the time components into the output char array
+  sprintf(output, "Year: %d",
+          year(timeInput));
 }
 
 // Initialize WiFi
@@ -65,8 +92,12 @@ void setup() {
 
 void loop() {
   epochTime = getTime();
+  time_t readableTimestamp = (time_t)epochTime;
+  char formattedTime[50];
+  printTime(readableTimestamp, formattedTime);
+
   Serial.print("Epoch Time: ");
   Serial.println(epochTime);
   delay(1000);
-  MQTT.publish("TccTagoIO/teste", "teste");
+  MQTT.publish("TccTagoIO/teste", formattedTime);
 }
